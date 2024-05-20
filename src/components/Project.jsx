30b1a5a9f5be92e535ae3cca93b4e8fe81d06b6d
wamandaka <108261088+wamandaka@users.noreply.github.com>
2024-05-20 23:12:56 +0700

    adjust link project

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -19,12 +19,16 @@ const Project = () => {
         <p className="text-sm font-medium tracking-[.35em] bg-clip-text text-transparent bg-gradient-to-r from-purple-600 to-orange-500">
           EXPLORE NOW
         </p>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-5 mx-4 my-10 md:mx-20 md:w-[70%]">
-          <a href="#" className="">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-5 mx-4 mt-10 mb-20 md:mx-20 md:w-[70%]">
+          <a
+            href="https://wamandaka.github.io/react-app-weatherapp/"
+            target="_blank"
+            rel="noreferrer"
+            className=""
+          >
             <Card className="bg-transparent text-white">
               <CardHeader>
                 <CardTitle>Weather App</CardTitle>
-                {/* <CardDescription>Card Description</CardDescription> */}
               </CardHeader>
               <CardContent>
                 <p>
@@ -32,67 +36,68 @@ const Project = () => {
                   OpenWeather API
                 </p>
               </CardContent>
-              <CardFooter className="flex justify-between">
-                {/* <p className="border px-4 py-1 rounded-full bg-slate-700">ReactJS</p>
-              <p className="border px-4 py-1 rounded-full">TailwindCSS</p>
-              <p className="border px-4 py-1 rounded-full">Axios</p> */}
+              <CardFooter className="flex justify-center items-center gap-2">
+                <p className="border px-4 py-1 rounded-full">ReactJS</p>
+                <p className="border px-4 py-1 rounded-full">TailwindCSS</p>
+                <p className="border px-4 py-1 rounded-full">Axios</p>
               </CardFooter>
             </Card>
           </a>
-          <a href="#" className="">
+          <a
+            href="https://personal-website-ayaka.vercel.app/"
+            target="_blank"
+            rel="noreferrer"
+            className=""
+          >
             <Card className="bg-transparent text-white">
               <CardHeader>
-                <CardTitle>Weather App</CardTitle>
-                {/* <CardDescription>Card Description</CardDescription> */}
+                <CardTitle>Personal Website</CardTitle>
               </CardHeader>
               <CardContent>
-                <p>
-                  Create a Weather App using ReactJS, TailwindCSS, Axios and
-                  OpenWeather API
-                </p>
+                <p>Slicing UI Personal Website with ReactJS and TailwindCSS</p>
               </CardContent>
-              <CardFooter className="flex justify-between">
-                {/* <p className="border px-4 py-1 rounded-full bg-slate-700">ReactJS</p>
-              <p className="border px-4 py-1 rounded-full">TailwindCSS</p>
-              <p className="border px-4 py-1 rounded-full">Axios</p> */}
+              <CardFooter className="flex justify-center items-center gap-2">
+                <p className="border px-4 py-1 rounded-full">ReactJS</p>
+                <p className="border px-4 py-1 rounded-full">TailwindCSS</p>
               </CardFooter>
             </Card>
           </a>
-          <a href="#" className="">
+          <a
+            href="https://reverse1999-characters-kang-piscok.koyeb.app/"
+            target="_blank"
+            rel="noreferrer"
+            className=""
+          >
             <Card className="bg-transparent text-white">
               <CardHeader>
-                <CardTitle>Weather App</CardTitle>
-                {/* <CardDescription>Card Description</CardDescription> */}
+                <CardTitle>Public API</CardTitle>
               </CardHeader>
               <CardContent>
-                <p>
-                  Create a Weather App using ReactJS, TailwindCSS, Axios and
-                  OpenWeather API
-                </p>
+                <p>Create a Public API (Reverse 1999 Character List)</p>
               </CardContent>
-              <CardFooter className="flex justify-between">
-                {/* <p className="border px-4 py-1 rounded-full bg-slate-700">ReactJS</p>
-              <p className="border px-4 py-1 rounded-full">TailwindCSS</p>
-              <p className="border px-4 py-1 rounded-full">Axios</p> */}
+              <CardFooter className="flex justify-center items-center gap-2">
+                <p className="border px-4 py-1 rounded-full">NodeJS</p>
+                <p className="border px-4 py-1 rounded-full">ExpressJS</p>
+                <p className="border px-4 py-1 rounded-full">PostgreSQL</p>
               </CardFooter>
             </Card>
           </a>
-          <a href="#" className="">
+          <a
+            href="https://slicing-saas-landing-page.vercel.app/"
+            target="_blank"
+            rel="noreferrer"
+            className=""
+          >
             <Card className="bg-transparent text-white">
               <CardHeader>
-                <CardTitle>Weather App</CardTitle>
-                {/* <CardDescription>Card Description</CardDescription> */}
+                <CardTitle>Landing Page</CardTitle>
               </CardHeader>
               <CardContent>
-                <p>
-                  Create a Weather App using ReactJS, TailwindCSS, Axios and
-                  OpenWeather API
-                </p>
+                <p>Slicing UI Landing Page with ReactJS and TailwindCSS</p>
               </CardContent>
-              <CardFooter className="flex justify-between">
-                {/* <p className="border px-4 py-1 rounded-full bg-slate-700">ReactJS</p>
-              <p className="border px-4 py-1 rounded-full">TailwindCSS</p>
-              <p className="border px-4 py-1 rounded-full">Axios</p> */}
+              <CardFooter className="flex justify-center items-center gap-2">
+                <p className="border px-4 py-1 rounded-full">ReactJS</p>
+                <p className="border px-4 py-1 rounded-full">TailwindCSS</p>
               </CardFooter>
             </Card>
           </a>
