8ac7872bafaa2cdbf4d71576e4b8ac85bc3fd8c6
wamandaka <108261088+wamandaka@users.noreply.github.com>
2024-05-20 23:15:50 +0700

    input link social media in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,13 +6,25 @@ const Footer = () => {
     <>
       <div className="w-full h-10 mb-10 space-y-3">
         <div className="flex justify-center items-center text-white gap-6">
-          <a href="">
+          <a
+            href="https://github.com/wamandaka"
+            target="_blank"
+            rel="noreferrer"
+          >
             <FaGithub size={30} />
           </a>
-          <a href="">
+          <a
+            href="https://www.instagram.com/kang_piscokk/"
+            target="_blank"
+            rel="noreferrer"
+          >
             <FaInstagram size={30} />
           </a>
-          <a href="">
+          <a
+            href="https://www.linkedin.com/in/wamandaka/"
+            target="_blank"
+            rel="noreferrer"
+          >
             <FaLinkedin size={30} />
           </a>
         </div>
