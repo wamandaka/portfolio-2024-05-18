5a387b40f9cae82164369466148db1b483d7d4cf
wamandaka <108261088+wamandaka@users.noreply.github.com>
2024-07-06 20:04:21 +0700

    di bikin DRY

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -9,6 +9,55 @@ import {
 } from "./ui/card";
 import { Button } from "./ui/button";
 
+const dataProjects = [
+  {
+    id: 1,
+    name: "Weather App",
+    url: "https://wamandaka.github.io/react-app-weatherapp/",
+    description:
+      "Create a Weather App using ReactJS, TailwindCSS, Axios and OpenWeather API",
+    technologies: [
+      { name: "ReactJS", color: "blue" },
+      { name: "TailwindCSS", color: "red" },
+      { name: "Axios", color: "text-red-500" },
+    ],
+  },
+  {
+    id: 2,
+    name: "REST API Todo List",
+    url: "https://github.com/wamandaka/cdp-mock-test",
+    description:
+      "Create a REST API Todo List Application for Mock Technical Test",
+    technologies: [
+      { name: "NodeJS", color: "text-green-700" },
+      { name: "ExpressJS", color: "text-yellow-500" },
+      { name: "PostgreSQL", color: "text-blue-800" },
+    ],
+  },
+  {
+    id: 3,
+    name: "Public API",
+    url: "https://reverse1999-characters-kang-piscok.koyeb.app/",
+    description: "Create a Public API (Reverse 1999 Character List)",
+    technologies: [
+      { name: "NodeJS", color: "text-green-700" },
+      { name: "ExpressJS", color: "text-yellow-500" },
+      { name: "PostgreSQL", color: "text-blue-800" },
+    ],
+  },
+  {
+    id: 4,
+    name: "Slicing Saas Landing Page",
+    url: "https://slicing-saas-landing-page.vercel.app/",
+    description: "Slicing UI Landing Page with ReactJS and TailwindCSS",
+    technologies: [
+      { name: "ReactJS", color: "text-blue-500" },
+      { name: "TailwindCSS", color: "text-green-500" },
+      { name: "daisyUI", color: "text-pink-500" },
+    ],
+  },
+];
+
 const Project = () => {
   return (
     <>
@@ -20,91 +69,40 @@ const Project = () => {
           EXPLORE NOW
         </p>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-5 mx-4 mt-10 mb-20 md:mx-20 md:w-[90%] lg:w-[60%]">
-          <a
-            href="https://wamandaka.github.io/react-app-weatherapp/"
-            target="_blank"
-            rel="noreferrer"
-            className=""
-          >
-            <Card className="bg-transparent text-white  hover:shadow-lg hover:shadow-purple-500/50 transition-all duration-300">
-              <CardHeader>
-                <CardTitle>Weather App</CardTitle>
-              </CardHeader>
-              <CardContent>
-                <p>
-                  Create a Weather App using ReactJS, TailwindCSS, Axios and
-                  OpenWeather API
-                </p>
-              </CardContent>
-              <CardFooter className="flex justify-center items-center gap-2">
-                <p className="border px-4 py-1 rounded-full">ReactJS</p>
-                <p className="border px-4 py-1 rounded-full">TailwindCSS</p>
-                <p className="border px-4 py-1 rounded-full">Axios</p>
-              </CardFooter>
-            </Card>
-          </a>
-          <a
-            href="https://github.com/wamandaka/cdp-mock-test"
-            target="_blank"
-            rel="noreferrer"
-            className=""
-          >
-            <Card className="bg-transparent text-white hover:shadow-lg hover:shadow-purple-500/50 transition-all duration-300">
-              <CardHeader>
-                <CardTitle>REST API Todo List</CardTitle>
-              </CardHeader>
-              <CardContent>
-                <p>
-                  Create a REST API Todo List Application for Mock Technical
-                  Test
-                </p>
-              </CardContent>
-              <CardFooter className="flex justify-center items-center gap-2">
-                <p className="border px-4 py-1 rounded-full">NodeJS</p>
-                <p className="border px-4 py-1 rounded-full">ExpressJS</p>
-                <p className="border px-4 py-1 rounded-full">PostgreSQL</p>
-              </CardFooter>
-            </Card>
-          </a>
-          <a
-            href="https://reverse1999-characters-kang-piscok.koyeb.app/"
-            target="_blank"
-            rel="noreferrer"
-            className=""
-          >
-            <Card className="bg-transparent text-white  hover:shadow-lg hover:shadow-purple-500/50 transition-all duration-300">
-              <CardHeader>
-                <CardTitle>Public API</CardTitle>
-              </CardHeader>
-              <CardContent>
-                <p>Create a Public API (Reverse 1999 Character List)</p>
-              </CardContent>
-              <CardFooter className="flex justify-center items-center gap-2">
-                <p className="border px-4 py-1 rounded-full">NodeJS</p>
-                <p className="border px-4 py-1 rounded-full">ExpressJS</p>
-                <p className="border px-4 py-1 rounded-full">PostgreSQL</p>
-              </CardFooter>
-            </Card>
-          </a>
-          <a
-            href="https://slicing-saas-landing-page.vercel.app/"
-            target="_blank"
-            rel="noreferrer"
-            className=""
-          >
-            <Card className="bg-transparent text-white  hover:shadow-lg hover:shadow-purple-500/50 transition-all duration-300">
-              <CardHeader>
-                <CardTitle>Landing Page</CardTitle>
-              </CardHeader>
-              <CardContent>
-                <p>Slicing UI Landing Page with ReactJS and TailwindCSS</p>
-              </CardContent>
-              <CardFooter className="flex justify-center items-center gap-2">
-                <p className="border px-4 py-1 rounded-full">ReactJS</p>
-                <p className="border px-4 py-1 rounded-full">TailwindCSS</p>
-              </CardFooter>
-            </Card>
-          </a>
+          {dataProjects.map((project) => {
+            return (
+              <a
+                href={project.url}
+                target="_blank"
+                rel="noreferrer"
+                className=""
+                key={project.id}
+              >
+                <Card className="bg-transparent group text-white hover:shadow-lg hover:shadow-purple-500/50 transition-all duration-300">
+                  <CardHeader>
+                    <CardTitle className="group-hover:bg-clip-text group-hover:text-transparent group-hover:bg-gradient-to-r group-hover:from-purple-600 group-hover:to-orange-500 group-hover:transition-all group-hover:duration-300 pb-1 group-hover:ease-in">
+                      {project.name}
+                    </CardTitle>
+                  </CardHeader>
+                  <CardContent>
+                    <p>{project.description}</p>
+                  </CardContent>
+                  <CardFooter className="flex justify-center items-center gap-2">
+                    {project.technologies.map((technology, index) => {
+                      return (
+                        <p
+                          key={index}
+                          className={`border px-4 py-1 rounded-full hover:text-${technology.color}-500`}
+                        >
+                          {technology.name}
+                        </p>
+                      );
+                    })}
+                  </CardFooter>
+                </Card>
+              </a>
+            );
+          })}
         </div>
       </div>
     </>
