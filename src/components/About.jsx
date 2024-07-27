fc808ab75afb4271c61ce4ba187e2d2df18a62bb
wamandaka <108261088+wamandaka@users.noreply.github.com>
2024-07-27 10:41:11 +0700

    using &apos;

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,14 +9,14 @@ const About = () => {
           EXPLORE NOW
         </p>
         <p className="text-sm md:text-lg font-medium mt-10 w-72 md:w-1/2 lg:w-[75%]">
-          I'm a Bachelor of Computer Science and a graduate from Binar Academy
-          Bootcamp stack Backend Javascript. Having good knowladge and skill
-          working with various frameworks and tools through my projects.
+          I&apos;m a Bachelor of Computer Science and a graduate from Binar
+          Academy Bootcamp stack Backend Javascript. Having good knowladge and
+          skill working with various frameworks and tools through my projects.
         </p>
         <p className="text-sm md:text-lg font-medium mt-7 w-72 md:w-1/2 lg:w-[75%]">
           I have experience in developing Landing Pages using ReactJS and
-          developing API using ExpressJS. Currently, I'm seeking an opportunity
-          to continue growing my skill set in the Full-Stack field.
+          developing API using ExpressJS. Currently, I&apos;m seeking an
+          opportunity to continue growing my skill set in the Full-Stack field.
         </p>
       </div>
     </>
