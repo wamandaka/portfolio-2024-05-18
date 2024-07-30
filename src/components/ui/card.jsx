98d0377bafe52a6b45bb95c9893537a0590aa000
wamandaka <108261088+wamandaka@users.noreply.github.com>
2024-07-30 13:49:11 +0700

    create a reuse card list component

--- src/components/ui/card.jsx
+++ src/components/ui/card.jsx
@@ -1,50 +1,70 @@
-import * as React from "react"
+import React from "react";
+import { FaGithub, FaPlayCircle, FaFigma } from "react-icons/fa";
+import { NavLink } from "react-router-dom";
 
-import { cn } from "@/lib/utils"
+const CardList = (props) => {
+  const { id, img, name, description, technologies, github, url, design } =
+    props;
+  return (
+    <div>
+      <div key={id} className="card card-compact bg-base-100 shadow-xl">
+        <figure className="w-full h-32">
+          <img
+            src={img}
+            alt={name}
+            className="object-cover transition duration-500 hover:scale-125"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title bg-clip-text text-transparent bg-gradient-to-r from-purple-600 to-orange-500 font-bold text-start truncate">
+            {name}
+          </h2>
+          <p className="text-start">{description}</p>
+          <div className="flex gap-2">
+            {technologies.map((tech, index) => {
+              return (
+                <div
+                  key={index}
+                  className="flex w-7 h-7 items-center justify-center bg-purple-700 rounded-md"
+                >
+                  <span className="text-xl font-medium">{tech}</span>
+                </div>
+              );
+            })}
+          </div>
 
-const Card = React.forwardRef(({ className, ...props }, ref) => (
-  <div
-    ref={ref}
-    className={cn("rounded-lg border bg-card text-card-foreground shadow-sm", className)}
-    {...props} />
-))
-Card.displayName = "Card"
+          <div className="card-actions mt-2 flex-nowrap gap-1">
+            <NavLink
+              to={github}
+              target="_blank"
+              rel="noreferrer"
+              className="btn btn-sm bg-orange-500 hover:bg-orange-600 text-white"
+            >
+              Repo <FaGithub size={17} />
+            </NavLink>
+            <NavLink
+              to={url}
+              target="_blank"
+              rel="noreferrer"
+              className="btn btn-sm bg-orange-500 hover:bg-orange-600 text-white"
+            >
+              Demo <FaPlayCircle size={17} />
+            </NavLink>
+            {design && (
+              <NavLink
+                to={design}
+                target="_blank"
+                rel="noreferrer"
+                className="btn btn-sm bg-orange-500 hover:bg-orange-600 text-white"
+              >
+                Design <FaFigma size={17} />
+              </NavLink>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-const CardHeader = React.forwardRef(({ className, ...props }, ref) => (
-  <div
-    ref={ref}
-    className={cn("flex flex-col space-y-1.5 p-6", className)}
-    {...props} />
-))
-CardHeader.displayName = "CardHeader"
-
-const CardTitle = React.forwardRef(({ className, ...props }, ref) => (
-  <h3
-    ref={ref}
-    className={cn("text-2xl font-semibold leading-none tracking-tight", className)}
-    {...props} />
-))
-CardTitle.displayName = "CardTitle"
-
-const CardDescription = React.forwardRef(({ className, ...props }, ref) => (
-  <p
-    ref={ref}
-    className={cn("text-sm", className)}
-    {...props} />
-))
-CardDescription.displayName = "CardDescription"
-
-const CardContent = React.forwardRef(({ className, ...props }, ref) => (
-  <div ref={ref} className={cn("p-6 pt-0", className)} {...props} />
-))
-CardContent.displayName = "CardContent"
-
-const CardFooter = React.forwardRef(({ className, ...props }, ref) => (
-  <div
-    ref={ref}
-    className={cn("flex items-center p-6 pt-0", className)}
-    {...props} />
-))
-CardFooter.displayName = "CardFooter"
-
-export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent }
+export default CardList;
